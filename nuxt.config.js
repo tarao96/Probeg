c3d40c56635a8fd589cc2c3255b7738c9cb49af3
skeito <s.keito896@gmail.com>
2023-02-13 00:48:22 +0900

    swiper削除

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,6 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/filter.js',
-    { src: '~/plugins/VueAwesomeSwiper', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
