cb15ff31acfa2cdfd27bb708f3462bccd30a506b
skeito <s.keito896@gmail.com>
2023-04-08 14:16:27 +0900

    description追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: '初学者にもわかりやすくをモットーにプログラミングの技術情報を発信しています。確実なスキルアップにつながるように丁寧に記事作成しています。' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
