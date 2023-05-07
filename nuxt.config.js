e66e2a96ef217fc8d0b12fd9f3af4400f8e54c69
skeito <s.keito896@gmail.com>
2023-05-07 23:01:58 +0900

    サーバーサイドレンダリングに変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  ssr: true,
+  target: 'server',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
